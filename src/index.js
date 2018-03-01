9b126bd6c1332425d12b7f6d082a4c3b9226eca4
VasiliyRepin <36683162+VasiliyRepin@users.noreply.github.com>
2018-03-01 14:36:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  
+        var sum =0;
+        while (number >= 5)
+        {
+            number = parseInt(number / 5);
+            sum = sum + number;
+        }
+            return sum;
 }
